f598e4cd73ffa27959a5278a6633bdac01e889fb
AyzleeR <julien_mercourt@hotmail.fr>
2023-06-13 12:08:51 +0200

    update des images du carrousel fonctionnel

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,14 @@
+import pool from "../config/database.js";
 
 export default (req, res) => {
-    res.render('layout', {template: 'index'});
+    pool.query(
+        "SELECT img FROM carrousel", function (error, img, result) {
+          if (error) {
+            console.error(error);
+            res.status(500).send("Erreur de base de données");
+          } else {
+            res.render('layout', {template: 'index', img: img});
+          }
+        }
+      );
 }
