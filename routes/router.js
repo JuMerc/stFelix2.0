494e1656da01168e7b631011b9b65bf587cdb107
AyzleeR <julien_mercourt@hotmail.fr>
2023-06-29 11:03:34 +0200

    nav admin et finalisation complete du site (ready)

--- routes/router.js
+++ routes/router.js
@@ -48,84 +48,87 @@ router.post('/login', LoginSubmit);
 router.get('/logout', Logout);
 
 //Affiche la page admin
-// router.get('/admin', adminCheckMiddleware(['main', 'second']) ,AdminController)
-router.get('/admin' ,AdminController);
+router.get('/admin', adminCheckMiddleware(['main', 'second']) ,AdminController)
+// router.get('/admin' ,AdminController);
 
 //Affiche la page pour ajouter un nouvel admin
-// router.get('/addnewadmin',adminCheckMiddleware(['main']),NewAdmin)
-router.get('/addnewadmin',NewAdmin);
+router.get('/addnewadmin',adminCheckMiddleware(['main']),NewAdmin)
+// router.get('/addnewadmin',NewAdmin);
 
 //Ajout d'un nouvel administrateur a l'aide du formulaire
-// router.post('/addnewadmin',  adminCheckMiddleware(['main']) ,AddNewAdmin);
-router.post('/addnewadmin',AddNewAdmin);
+router.post('/addnewadmin',  adminCheckMiddleware(['main']) ,AddNewAdmin);
+// router.post('/addnewadmin',AddNewAdmin);
 
 //Modification d'un admin
-router.post('/updateadmin',UpdateAdmin);
+router.post('/updateadmin',adminCheckMiddleware(['main']) ,UpdateAdmin);
+// router.post('/updateadmin',UpdateAdmin);
 
 //Suppression d'un admin secondaire
-// router.delete('/admin/:id',  adminCheckMiddleware(['main']) ,DeleteAdmin);
-router.delete('/admin/:id' ,DeleteAdmin);
+router.delete('/admin/:id',  adminCheckMiddleware(['main']) ,DeleteAdmin);
+// router.delete('/admin/:id' ,DeleteAdmin);
 
 //Update des photos dans le carrousel via un formulaire
-router.post('/addcarrouselpicture', UpdateCarrouselPicture);
+// router.post('/addcarrouselpicture', UpdateCarrouselPicture);
+router.post('/addcarrouselpicture',adminCheckMiddleware(['main']) , UpdateCarrouselPicture);
 
 //Update du text dans l'accueil
-// router.post('/updateindextext',  adminCheckMiddleware(['main']) ,UpdateIndexText);
-router.post('/updateindextext',UpdateIndexText);
+router.post('/updateindextext',  adminCheckMiddleware(['main']) ,UpdateIndexText);
+// router.post('/updateindextext',UpdateIndexText);
 
 //Ajout d'une marque dans la page d'accueil
-// router.post('/addbrand',  adminCheckMiddleware(['main']) ,AddBrand);
-router.post('/addbrand',AddBrand);
+router.post('/addbrand',  adminCheckMiddleware(['main']) ,AddBrand);
+// router.post('/addbrand',AddBrand);
 
 //Suppression d'une marque
-// router.delete('/brand/:id',  adminCheckMiddleware(['main']) ,DeleteBrand);
-router.delete('/brand/:id',DeleteBrand);
+router.delete('/brand/:id',  adminCheckMiddleware(['main']) ,DeleteBrand);
+// router.delete('/brand/:id',DeleteBrand);
 
 //Ajout d'une catégorie dans la page prestation
-// router.post('/addcategory',  adminCheckMiddleware(['main']) ,AddCategory);
-router.post('/addcategory',AddCategory);
+router.post('/addcategory',  adminCheckMiddleware(['main']) ,AddCategory);
+// router.post('/addcategory',AddCategory);
 
 //Ajout d'une prestation et d'un prix liés à une catégorie
-// router.post('/addbenefit',  adminCheckMiddleware(['main']) ,AddBenefit);
-router.post('/addbenefit',AddBenefit);
+router.post('/addbenefit',  adminCheckMiddleware(['main']) ,AddBenefit);
+// router.post('/addbenefit',AddBenefit);
 
 //Modification d'un catégorie dans la page prestation
-router.post('/updatecategory',UpdateCategory);
-// router.post('/updatecategory',  adminCheckMiddleware(['main']) ,UpdateCategory);
+// router.post('/updatecategory',UpdateCategory);
+router.post('/updatecategory',  adminCheckMiddleware(['main']) ,UpdateCategory);
 
 // Modification d'une prestation
-// router.post('/updatebenefit',  adminCheckMiddleware(['main']) ,UpdateBenefit);
-router.post('/updatebenefit' ,UpdateBenefit);
+router.post('/updatebenefit',  adminCheckMiddleware(['main']) ,UpdateBenefit);
+// router.post('/updatebenefit' ,UpdateBenefit);
 
 //Suppression d'une catégorie de prestation
-// router.delete('/category/:id', adminCheckMiddleware(['main']) , DeleteCategory);
-router.delete('/category/:id', DeleteCategory);
+router.delete('/category/:id', adminCheckMiddleware(['main']) , DeleteCategory);
+// router.delete('/category/:id', DeleteCategory);
 
 //Suppression d'une prestation
-// router.delete('/benefit/:id', adminCheckMiddleware(['main']) , DeleteBenefit);
-router.delete('/benefit/:id',  DeleteBenefit);
+router.delete('/benefit/:id', adminCheckMiddleware(['main']) , DeleteBenefit);
+// router.delete('/benefit/:id',  DeleteBenefit);
 
 //Ajout d'une photo dans la galerie
-// router.post('/addpictureingallery',  adminCheckMiddleware(['main', 'second']) ,AddPictureInGallery);
-router.post('/addpictureingallery',  AddPictureInGallery);
+router.post('/addpictureingallery',  adminCheckMiddleware(['main', 'second']) ,AddPictureInGallery);
+// router.post('/addpictureingallery',  AddPictureInGallery);
 
 //Suppression d'une photo dans la galerie
-// router.delete('/picture/:id', adminCheckMiddleware(['main', 'second']) , DeletePictureInGallery);
-router.delete('/picture/:id',  DeletePictureInGallery);
+router.delete('/picture/:id', adminCheckMiddleware(['main', 'second']) , DeletePictureInGallery);
+// router.delete('/picture/:id',  DeletePictureInGallery);
 
 //Modification des informations diverses
-// router.post('/updateinfos', adminCheckMiddleware(['main', 'second']) , UpdateInfos)
-router.post('/updateinfos',  UpdateInfos)
+router.post('/updateinfos', adminCheckMiddleware(['main', 'second']) , UpdateInfos)
+// router.post('/updateinfos',  UpdateInfos)
 
 //Modification d'un horaire
-router.post('/updateschedule', UpdateSchedule)
+// router.post('/updateschedule', UpdateSchedule)
+router.post('/updateschedule',adminCheckMiddleware(['main', 'second']) ,UpdateSchedule)
 
 //Ajout d'un nouvel horaire
-// router.post('/addnewschedule', adminCheckMiddleware(['main']) , AddNewSchedule)
-router.post('/addnewschedule',  AddNewSchedule)
+router.post('/addnewschedule', adminCheckMiddleware(['main']) , AddNewSchedule)
+// router.post('/addnewschedule',  AddNewSchedule)
 
 //Suppression d'un horaire
-// router.delete('/schedule/:id', adminCheckMiddleware(['main']) , DeleteSchedule);
-router.delete('/schedule/:id',  DeleteSchedule);
+router.delete('/schedule/:id', adminCheckMiddleware(['main']) , DeleteSchedule);
+// router.delete('/schedule/:id',  DeleteSchedule);
 
 export default router;
